e5e675a7adae051e042de6c5c08cee1eb7743a16
DireSafelane <parncomado@outlook.com>
2024-07-23 15:26:57 +0700

    Edit string_operation and readme

---
+++ assignment4/main.py
@@ -0,0 +1,3 @@
+import string_operations
+sample =  "hello World"
+print(string_operations.reverse_string(sample),string_operations.capitalize_string(sample),string_operations.lowercase_string(sample),string_operations.uppercase_string(sample))

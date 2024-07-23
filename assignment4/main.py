d809a8009afa551f56fcd2c801f1aaa90bee326e
DireSafelane <parncomado@outlook.com>
2024-07-23 15:43:30 +0700

    change main

--- assignment4/main.py
+++ assignment4/main.py
@@ -1,3 +1,7 @@
 import string_operations
 sample =  "hello World"
-print(string_operations.reverse_string(sample),string_operations.capitalize_string(sample),string_operations.lowercase_string(sample),string_operations.uppercase_string(sample))
+
+print(string_operations.reverse_string(sample))
+print(string_operations.capitalize_string(sample))
+print(string_operations.lowercase_string(sample))
+print(string_operations.uppercase_string(sample))
